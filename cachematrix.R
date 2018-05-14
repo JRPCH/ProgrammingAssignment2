478b6b8bb6b4fed001bed27e0ea77009628fb2a7
JRPCH <38642393+JRPCH@users.noreply.github.com>
2018-05-14 00:53:12 -0600

    Add files via upload
    Commit your completed R file into my git repository and push your git branch to the GitHub repository under your account.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two function that cache and solve the inverse of a matrix.
+## 
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  cache_inv <- NULL
+  
+  ## The matrix set method
+  set <- function( matrix) {
+    x   <<- matrix
+    cache_inv <<- NULL
+  }
+    
+  ## The get method
+  get <- function() {
+    x
+  }
+  
+  ## The inverse set method
+  setInverse <- function (inverse) {
+    cache_inv <<- inverse
+  }
+  
+  ## The inverse get method
+  getInverse <- function () {
+    cache_inv
+  }
+  
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+  
 
 }
 
-
-## Write a short comment describing this function
+# cacheSolve: This function computes the inverse of the special "matrix" returned
+# by makeCacheMatrix above. If the inverse has already been calculated 
+# and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv_matrix_x <-x$getInverse()
+    
+    # Return the cached matrix
+    if(!is.null(inv_matrix_x)) {
+      message("getting cached data")
+      return(inv_matrix_x)
+    }
+    
+    # Set data getting the matrix object
+    matrix_x <- x$get()
+    
+    inv_matrix_x <- solve(matrix_x,...)
+    
+    # Set the inverse matrix to the object
+    
+    x$setInverse(inv_matrix_x)
+    
+    # Return inv_matrix_x the matrix
+    inv_matrix_x
 }
+
+ 
